37aef7cefb6ae94f4b6bdba604cf8d349da843ed
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-02-25 23:47:01 -0600

    Added login registration and rg deletion

--- debianVmLab.sh
+++ debianVmLab.sh
@@ -1,3 +1,6 @@
+# Registering Azure account
+az login
+
 # Making the resource group
 az group create --location eastus --resource-group debianLab
 
@@ -6,4 +9,8 @@ az vm create \
     --resource-group debianLab  \
     --name debian \
     --image debian11 \
-   --authentication-type Password  --admin-username <username>
+   --authentication-type Password  --admin-username username
+
+# Deleting the resource group
+az group delete --name debianLab --yes --no-wait
+az group delete --name NetworkWatcherRG --yes --no-wait
