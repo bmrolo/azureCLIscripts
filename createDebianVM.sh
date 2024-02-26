ce8918ec564f55969295df8e9ff38365be2a98a8
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-02-25 23:30:14 -0600

    Update createDebianVM.sh

--- createDebianVM.sh
+++ createDebianVM.sh
@@ -1,6 +1,9 @@
-az vm create \
-    --resource-group Linux_group \
+# Making the resource group
+az group create --location eastus --resource-group debianLab
+
+# Making the vm
+az vm create \                                               
+    --resource-group debianLab  \
     --name debian \
     --image debian11 \
-    --admin-username bmrolo
-    --admin-password P@ssw0rd-123
+   --authentication-type Password  --admin-username bmrolo
